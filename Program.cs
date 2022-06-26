eef7a9524fcbe686e493bb8321eeb26482948dae
dygnyc <donaldyigeorge@gmail.com>
2022-06-25 21:52:50 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿Random random = new Random();
+
+Dictionary<string,string> quizTerms = new Dictionary<string,string>();
+// first string is key / term, second string is value / definition
+
+//TestDictionary();
+
+ReadStream();
+
+//main loop
+while (quizTerms.Count > 0)
+{
+    Console.Clear();
+    Console.WriteLine(quizTerms.Count() + " terms left");
+
+    int index;
+    index = AskQuestion();
+
+    CheckCorrect(index);
+}
+
+int AskQuestion()
+{
+    int index = random.Next(quizTerms.Count);
+
+    string question = quizTerms.Keys.ElementAt(index);
+    string answer = quizTerms.Values.ElementAt(index);
+
+    Console.WriteLine("\nWhat is: " + question);
+    Console.WriteLine("\n<press enter for answer>");
+    Console.ReadLine();
+    Console.WriteLine(answer +"\n");
+
+    return index;
+}
+
+
+void CheckCorrect(int index)
+{
+    while (true)
+    {
+        Console.WriteLine("Did you get it correct? (y/n)");
+
+        string correct = Console.ReadLine();
+
+        if (correct == "y")
+        {
+            //Console.WriteLine("Correct");          
+            // remove from array
+
+            if (quizTerms.Count() == 1)
+            {
+                Console.WriteLine("\nWell done!");
+                quizTerms.Remove(quizTerms.Keys.ElementAt(index));
+                break;
+            }
+            else
+            {
+                quizTerms.Remove(quizTerms.Keys.ElementAt(index));
+                break;
+            }
+        }
+        else if (correct == "n")
+        {
+            Console.WriteLine("Wrong");
+
+            //do not remove question from array
+   
+            break;
+        }
+
+
+    }
+}
+
+void ReadStream()
+{
+    StreamReader sr = new StreamReader("..\\..\\..\\rootTerms.txt");
+
+    string term = sr.ReadLine();
+    string definition = sr.ReadLine();
+    string blank = sr.ReadLine();
+
+    quizTerms.Add(term, definition);
+
+    while ((term = sr.ReadLine()) != null)
+    {
+        definition = sr.ReadLine();
+        blank = sr.ReadLine();
+        quizTerms.Add(term, definition);
+    }
+
+    sr.Close();
+}
+
+void TestDictionary()
+{
+    quizTerms.Add("hyster-", "pertaining to uterus");
+    quizTerms.Add("my(o)", "muscle");
+}
+
